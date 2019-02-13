cc3dc56e7ffef48dd0f4a5c807d91d883787c3aa
Jenya Y <jenyay@gmail.com>
2019-02-14 00:26:25 +0200

    Set config file for tests

--- tests/Integration/Utils/TestsContext.cs
+++ tests/Integration/Utils/TestsContext.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using System;
 using Web;
 
 namespace Integration.Utils
 {
     public class TestsContext : WebApplicationFactory<Startup>
     {
+        protected override IWebHostBuilder CreateWebHostBuilder()
+        {
+            return base.CreateWebHostBuilder()
+                .UseEnvironment("Test")
+                .ConfigureAppConfiguration((c, b) =>
+                {
+                    b
+                    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                    .AddJsonFile("appsettings.json", optional: false);
+                });
+        }
+
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             builder.ConfigureServices(services =>
